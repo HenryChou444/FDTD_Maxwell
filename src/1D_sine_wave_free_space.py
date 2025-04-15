9dc3f9bdd29e95eec66ab21ae58b8fa2b36ec386
henrychou444 <henry.yuheng.chou@ulb.be>
2025-04-15 18:10:54 +0200

    step 1 a

--- src/1D_sine_wave_free_space.py
+++ src/1D_sine_wave_free_space.py
@@ -9,7 +9,7 @@ f = 2.4e9  # Frequency (Hz)
 Lambda = c / f  # Wavelength (m)
 dx = Lambda / 20  # Spatial step (m)
 dt = dx / (2 * c)  # Time step (s)
-M = 1000  # Number of space steps
+M = 100  # Number of space steps
 Q = 1000  # Number of time steps
 x = np.linspace(0, (M - 1) * dx, M)  # Space grid
 t = np.linspace(0, (Q - 1) * dt, Q)  # Time grid
@@ -21,9 +21,28 @@ J = np.zeros((Q, M))  # Current density
 source_position = M // 2  # Position of the source (int)
 J[:, source_position] = np.sin(omega * t)  # Sine wave source
 
+# Create E field
+E = np.zeros((Q, M))  # Electric field
+B = np.zeros((Q, M))  # Magnetic field
+for q in range(1, Q): # B_(q'+1/2) [m' + 1/2 = B_q [m]
+    for m in range(1, M - 1):
+        E[q, m] = E[q - 1, m] + 1/2 *(B[q-1, m]- B[q-1, m-1]) - (dt / epsilon_0) * (J[q-1,m])
+#   # Boundary conditions
+    if q > 1 :
+        E[q, 1] = E[q-2, 2]
+        E[q, M-1] =  E[q-2, M-2]
+    else :
+        E[q, 1] = 0
+        E[q, M-1] = 0
+
+    for m in range(1, M - 1):
+        B[q, m] = B[q - 1,m] + 1/2 *(E[q, m+1] - E[q, m])    
+
+
+
 # Initialize the figure
 fig, ax = plt.subplots()
-animated_source, = ax.plot([], [], 'o', markersize=4, color='red')  # Sine wave source
+animated_source, = ax.plot([], [])  # E
 ax.set_xlim(0, (M - 1) * dx)
 ax.set_ylim(-1.5, 1.5)
 ax.set_xlabel("x [m]")
@@ -31,12 +50,16 @@ ax.set_ylabel("E [V/m]")
 ax.set_title("1D FDTD")
 
 # Animation function
+#animated_source.set_data([x[source_position]], [J[frame, source_position]])  # Update source
 def update(frame):
-    animated_source.set_data([x[source_position]], [J[frame, source_position]])  # Update source
+    animated_source.set_data(x, E[frame, :]) # Update E field
     return animated_source,
 
 # Create the animation
 ani = FuncAnimation(fig, update, frames=Q, interval=5, blit=True)
+
+# Save the animation
+ani.save("1D_sine_source_free_space.mp4", fps=60)
 
 # Show the animation
 plt.show()
