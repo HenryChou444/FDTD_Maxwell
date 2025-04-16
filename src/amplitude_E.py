06d4d85b98a2243e958eacaab75280ca2e2dab1f
henrychou444 <henry.yuheng.chou@ulb.be>
2025-04-16 20:44:32 +0200

    dx change ampltitude ??

---
+++ src/amplitude_E.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.constants import c
+
+
+E = np.load("E_matrix_1D_sine.npy")  # Load the electric field matrix
+# Parameters
+f = 2.4e9  # Frequency (Hz)
+Lambda = c / f  # Wavelength (m)
+dx = Lambda / 50  # Spatial step (m)
+a = 2
+dt = dx / (a * c)  # Time step (s)
+M = 150 # Number of space steps
+Q = 2000  # Number of time steps
+x = np.linspace(0, (M - 1) * dx, M)  # Space grid
+t = np.linspace(0, (Q - 1) * dt, Q)  # Time grid
+
+amplitude = np.zeros(M)  # Initialize amplitude array
+for m in range(0, M):
+    amplitude[m] = (np.max(E[int(Q/2) : , m]) - np.min(E[int(Q/2) :, m]))/2  
+    #amplitude[m] = np.max(E[int(Q/2) : , m])
+
+plt.figure()
+plt.plot(x, amplitude)
+plt.title("")
+plt.xlabel("x [m]", fontsize=14)
+plt.ylabel("Amplitude [V/m]", fontsize=14)
+plt.xlim(0, (M - 1) * dx)
+plt.ylim(0, 0.5)
+plt.grid()
+plt.show()
---
+++ src/animation_only_L50.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from scipy.constants import c
+
+
+E = np.load("E_matrix_1D_sine.npy")  # Load the electric field matrix
+# Parameters
+f = 2.4e9  # Frequency (Hz)
+Lambda = c / f  # Wavelength (m)
+dx = Lambda / 50  # Spatial step (m)
+a = 2
+dt = dx / (a * c)  # Time step (s)
+M = 150 # Number of space steps
+Q = 2000  # Number of time steps
+x = np.linspace(0, (M - 1) * dx, M)  # Space grid
+t = np.linspace(0, (Q - 1) * dt, Q)  # Time grid
+
+# Initialize the figure
+fig, ax = plt.subplots()
+animated_source, = ax.plot([], [])  # E
+ax.set_xlim(0, (M - 1) * dx)
+ax.set_ylim(-1.5, 1.5)
+ax.set_xlabel("x [m]")
+ax.set_ylabel("E [V/m]")
+ax.set_title("1D FDTD")
+
+# Animation function
+#animated_source.set_data([x[source_position]], [J[frame, source_position]])  # Update source
+def update(frame):
+    animated_source.set_data(x, E[frame, :]) # Update E field
+    return animated_source,
+
+# Create the animation
+ani = FuncAnimation(fig, update, frames=Q, interval=15, blit=True)
+
+
+# Show the animation
+plt.show()
